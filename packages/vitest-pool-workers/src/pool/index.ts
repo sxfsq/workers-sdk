b97f6dfd660cabc6e6b8d9107f3dc8f6f395a0d7
Andy Jessop <ajessop@cloudflare.com>
2024-12-17 10:18:29 +0100

    chore: remove v2 globals

--- packages/vitest-pool-workers/src/pool/index.ts
+++ packages/vitest-pool-workers/src/pool/index.ts
@@ -372,6 +372,7 @@ function buildProjectWorkerOptions(
 
 	if (mode !== "v1" && mode !== "v2") {
 		runnerWorker.compatibilityFlags.push("nodejs_compat");
+		runnerWorker.compatibilityFlags.push("no_nodejs_compat_v2");
 	}
 
 	// Required for `workerd:unsafe` module. We don't require this flag to be set
